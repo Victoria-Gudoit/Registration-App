195dc92a5489206de0990b6a5b09d57a1dcf1570
Victoria-Gudoit <vgudojt@mail.ru>
2022-04-16 16:57:06 +0300

    refactor: add uuid in Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,6 +3,7 @@ import css from "./modal.module.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { RegistrationCtx } from "./Ctx";
+import {v4 as uuid} from "uuid"
 
 export class Modal extends React.Component {
   static contextType = RegistrationCtx;
@@ -23,15 +24,13 @@ export class Modal extends React.Component {
     const values = [{label: 'Ваш логин:', value: this.context.login}, {label: 'Ваш пароль:', value: this.context.password}, {label: 'Ваш пол:', value: this.props.gender}]
     const modal = (
       <div key={this.props.onCloseModal} className={css.modal}>
-        <div className={css.info}>
         <ul className={css.list}>
           {values.map(item => {
             return (
-                   <li>{item.label} {item.value}</li>
+               <li key={uuid()}>{item.label} {item.value}</li>
             )
           })}
         </ul>
-        </div>
         <p>Регистрация прошла успешна!</p>
         <button onClick={this.props.onCloseModal} className={css.btn}>
           Хорошо
